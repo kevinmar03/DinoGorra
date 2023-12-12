9dcf8cdf6d7a0d1cf119c37cdda18c02bb9fc3d6
kevinmar03 <kevinmartinmartin03@gmail.com>
2023-12-12 14:36:47 +0100

    Commit (Script + animación personaje)

---
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            animator.SetBool("AccionSalto", true);
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            animator.SetBool("AccionSalto", false);
+        }
+    }
+}
