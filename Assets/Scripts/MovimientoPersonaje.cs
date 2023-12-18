6eaa55461b01634ef6093341f5b93b8fb970e779
kevinmar03 <kevinmartinmartin03@gmail.com>
2023-12-18 11:03:50 +0100

    Commit (Escalado del canvas + modificación script personaje)

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class MovimientoPersonaje : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] int alturaSalto;
+    [SerializeField] Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetButton("Jump"))
         {
             animator.SetBool("AccionSalto", true);
+            rigidbody.AddForce(Vector2.up * alturaSalto);
         }
-        if (Input.GetKeyUp(KeyCode.V))
+        else
         {
             animator.SetBool("AccionSalto", false);
+            rigidbody.velocity = Vector2.zero;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
