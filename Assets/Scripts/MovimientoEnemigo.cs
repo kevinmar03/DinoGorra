0bd6204403c0c82a8240449d1c84379468696ae8
DESKTOP-NM0RBMK\milag <edurinvaz@gmail.com>
2023-12-19 14:52:43 +0100

    Cambios

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovimientoEnemigo : MonoBehaviour
 {
     [SerializeField] Camera camara;
-    [SerializeField] Vector3 posicionMinima;
-    [SerializeField] Vector3 posicionMaxima;
+    [SerializeField] Vector3 posicionMinima, posicionInicial;
     [SerializeField] float velocidad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camara = Camera.main;
+        posicionMinima = camara.ViewportToWorldPoint(new Vector3(0, 0));
+        posicionInicial = camara.ViewportToWorldPoint(new Vector3(1, 1));
     }
 
     // Update is called once per frame
